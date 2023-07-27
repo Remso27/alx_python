fe5c9c6d0ac35f2dd036d3a335a7b09d9a8ee5d5
Remso27 <kouassiremi27@gmail.com>
2023-07-27 09:55:33 +0000

    Task4

---
+++ python-import_modules/4-raise_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def raise_exception():
+    a = 12
+    name = "Rémi"
+    try:
+        result = a + name
+        raise NameError ("Here")
+    except NameError as NE:
+        print("Exeption")
+        raise NE
